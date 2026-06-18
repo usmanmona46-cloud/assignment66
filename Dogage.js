91a3409716aa057b0bad52e6801e2e5f6b9ff551
mona <usmanmona46@gmail.com>
2026-06-18 11:12:42 +0100

    Add files via upload

---
+++ Dogage.js
@@ -0,0 +1,15 @@
+function calculateDogAge(puppyAge) {
+    let dogAge = puppyAge * 7;
+    console.log("Your doggie is " + dogAge + " years old in dog years!");
+}
+
+//
+function calcCircumference(radius) {
+    let circumference = 2 * Math.PI * radius;
+    console.log("The circumference is " + circumference);
+}
+
+
+
+
+
